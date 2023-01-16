aebfe3c56d2fffee7c676bd74db5444f56c544aa
Nafiz Imtiaz Khan <nafizimtiazkhan@gmail.com>
2023-01-16 13:07:29 +0600

    New

---
+++ String_Manipulation/FrmStringManipulation.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace String_Manipulation
+{
+    partial class FrmStringManipulation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtInput = new System.Windows.Forms.TextBox();
+            this.BtnLength = new System.Windows.Forms.Button();
+            this.TxtResult = new System.Windows.Forms.TextBox();
+            this.btnSubstring = new System.Windows.Forms.Button();
+            this.BtnSubstring34 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BtnStartsWith = new System.Windows.Forms.Button();
+            this.BtnEndswith = new System.Windows.Forms.Button();
+            this.BtnIndexof3 = new System.Windows.Forms.Button();
+            this.BtnToupper = new System.Windows.Forms.Button();
+            this.BtnTolower = new System.Windows.Forms.Button();
+            this.BtnReplace = new System.Windows.Forms.Button();
+            this.BtnVowels = new System.Windows.Forms.Button();
+            this.BtnSplit = new System.Windows.Forms.Button();
+            this.BtnNoofthe = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TxtInput
+            // 
+            this.TxtInput.Location = new System.Drawing.Point(23, 41);
+            this.TxtInput.Name = "TxtInput";
+            this.TxtInput.Size = new System.Drawing.Size(100, 20);
+            this.TxtInput.TabIndex = 0;
+            // 
+            // BtnLength
+            // 
+            this.BtnLength.Location = new System.Drawing.Point(23, 94);
+            this.BtnLength.Name = "BtnLength";
+            this.BtnLength.Size = new System.Drawing.Size(86, 23);
+            this.BtnLength.TabIndex = 1;
+            this.BtnLength.Text = "Length";
+            this.BtnLength.UseVisualStyleBackColor = true;
+            this.BtnLength.Click += new System.EventHandler(this.BtnLength_Click);
+            // 
+            // TxtResult
+            // 
+            this.TxtResult.Location = new System.Drawing.Point(240, 40);
+            this.TxtResult.Name = "TxtResult";
+            this.TxtResult.Size = new System.Drawing.Size(100, 20);
+            this.TxtResult.TabIndex = 2;
+            // 
+            // btnSubstring
+            // 
+            this.btnSubstring.Location = new System.Drawing.Point(143, 94);
+            this.btnSubstring.Name = "btnSubstring";
+            this.btnSubstring.Size = new System.Drawing.Size(75, 23);
+            this.btnSubstring.TabIndex = 3;
+            this.btnSubstring.Text = "Substring(4)";
+            this.btnSubstring.UseVisualStyleBackColor = true;
+            this.btnSubstring.Click += new System.EventHandler(this.btnSubstring_Click);
+            // 
+            // BtnSubstring34
+            // 
+            this.BtnSubstring34.Location = new System.Drawing.Point(258, 94);
+            this.BtnSubstring34.Name = "BtnSubstring34";
+            this.BtnSubstring34.Size = new System.Drawing.Size(82, 23);
+            this.BtnSubstring34.TabIndex = 4;
+            this.BtnSubstring34.Text = "Substring(3,4)";
+            this.BtnSubstring34.UseVisualStyleBackColor = true;
+            this.BtnSubstring34.Click += new System.EventHandler(this.BtnSubstring34_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Input";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(237, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Result";
+            // 
+            // BtnStartsWith
+            // 
+            this.BtnStartsWith.Location = new System.Drawing.Point(23, 134);
+            this.BtnStartsWith.Name = "BtnStartsWith";
+            this.BtnStartsWith.Size = new System.Drawing.Size(86, 23);
+            this.BtnStartsWith.TabIndex = 7;
+            this.BtnStartsWith.Text = "Starts With 1";
+            this.BtnStartsWith.UseVisualStyleBackColor = true;
+            this.BtnStartsWith.Click += new System.EventHandler(this.BtnStartsWith_Click);
+            // 
+            // BtnEndswith
+            // 
+            this.BtnEndswith.Location = new System.Drawing.Point(143, 134);
+            this.BtnEndswith.Name = "BtnEndswith";
+            this.BtnEndswith.Size = new System.Drawing.Size(75, 23);
+            this.BtnEndswith.TabIndex = 8;
+            this.BtnEndswith.Text = "Ends With 9";
+            this.BtnEndswith.UseVisualStyleBackColor = true;
+            this.BtnEndswith.Click += new System.EventHandler(this.BtnEndswith_Click);
+            // 
+            // BtnIndexof3
+            // 
+            this.BtnIndexof3.Location = new System.Drawing.Point(258, 133);
+            this.BtnIndexof3.Name = "BtnIndexof3";
+            this.BtnIndexof3.Size = new System.Drawing.Size(82, 23);
+            this.BtnIndexof3.TabIndex = 9;
+            this.BtnIndexof3.Text = "Index of 3";
+            this.BtnIndexof3.UseVisualStyleBackColor = true;
+            this.BtnIndexof3.Click += new System.EventHandler(this.BtnIndexof3_Click);
+            // 
+            // BtnToupper
+            // 
+            this.BtnToupper.Location = new System.Drawing.Point(23, 179);
+            this.BtnToupper.Name = "BtnToupper";
+            this.BtnToupper.Size = new System.Drawing.Size(86, 23);
+            this.BtnToupper.TabIndex = 10;
+            this.BtnToupper.Text = "To Upper";
+            this.BtnToupper.UseVisualStyleBackColor = true;
+            this.BtnToupper.Click += new System.EventHandler(this.BtnToupper_Click);
+            // 
+            // BtnTolower
+            // 
+            this.BtnTolower.Location = new System.Drawing.Point(143, 178);
+            this.BtnTolower.Name = "BtnTolower";
+            this.BtnTolower.Size = new System.Drawing.Size(75, 23);
+            this.BtnTolower.TabIndex = 11;
+            this.BtnTolower.Text = "To Lower";
+            this.BtnTolower.UseVisualStyleBackColor = true;
+            this.BtnTolower.Click += new System.EventHandler(this.BtnTolower_Click);
+            // 
+            // BtnReplace
+            // 
+            this.BtnReplace.Location = new System.Drawing.Point(258, 177);
+            this.BtnReplace.Name = "BtnReplace";
+            this.BtnReplace.Size = new System.Drawing.Size(82, 23);
+            this.BtnReplace.TabIndex = 12;
+            this.BtnReplace.Text = "Replace 1->9";
+            this.BtnReplace.UseVisualStyleBackColor = true;
+            this.BtnReplace.Click += new System.EventHandler(this.BtnReplace_Click);
+            // 
+            // BtnVowels
+            // 
+            this.BtnVowels.Location = new System.Drawing.Point(23, 221);
+            this.BtnVowels.Name = "BtnVowels";
+            this.BtnVowels.Size = new System.Drawing.Size(86, 23);
+            this.BtnVowels.TabIndex = 13;
+            this.BtnVowels.Text = "Vowels";
+            this.BtnVowels.UseVisualStyleBackColor = true;
+            this.BtnVowels.Click += new System.EventHandler(this.BtnVowels_Click);
+            // 
+            // BtnSplit
+            // 
+            this.BtnSplit.Location = new System.Drawing.Point(143, 221);
+            this.BtnSplit.Name = "BtnSplit";
+            this.BtnSplit.Size = new System.Drawing.Size(75, 23);
+            this.BtnSplit.TabIndex = 14;
+            this.BtnSplit.Text = "split";
+            this.BtnSplit.UseVisualStyleBackColor = true;
+            this.BtnSplit.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // BtnNoofthe
+            // 
+            this.BtnNoofthe.Location = new System.Drawing.Point(258, 220);
+            this.BtnNoofthe.Name = "BtnNoofthe";
+            this.BtnNoofthe.Size = new System.Drawing.Size(75, 23);
+            this.BtnNoofthe.TabIndex = 15;
+            this.BtnNoofthe.Text = "No. Of The";
+            this.BtnNoofthe.UseVisualStyleBackColor = true;
+            this.BtnNoofthe.Click += new System.EventHandler(this.BtnNoofthe_Click);
+            // 
+            // FrmStringManipulation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(367, 344);
+            this.Controls.Add(this.BtnNoofthe);
+            this.Controls.Add(this.BtnSplit);
+            this.Controls.Add(this.BtnVowels);
+            this.Controls.Add(this.BtnReplace);
+            this.Controls.Add(this.BtnTolower);
+            this.Controls.Add(this.BtnToupper);
+            this.Controls.Add(this.BtnIndexof3);
+            this.Controls.Add(this.BtnEndswith);
+            this.Controls.Add(this.BtnStartsWith);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnSubstring34);
+            this.Controls.Add(this.btnSubstring);
+            this.Controls.Add(this.TxtResult);
+            this.Controls.Add(this.BtnLength);
+            this.Controls.Add(this.TxtInput);
+            this.Name = "FrmStringManipulation";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "String Manipulation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtInput;
+        private System.Windows.Forms.Button BtnLength;
+        private System.Windows.Forms.TextBox TxtResult;
+        private System.Windows.Forms.Button btnSubstring;
+        private System.Windows.Forms.Button BtnSubstring34;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button BtnStartsWith;
+        private System.Windows.Forms.Button BtnEndswith;
+        private System.Windows.Forms.Button BtnIndexof3;
+        private System.Windows.Forms.Button BtnToupper;
+        private System.Windows.Forms.Button BtnTolower;
+        private System.Windows.Forms.Button BtnReplace;
+        private System.Windows.Forms.Button BtnVowels;
+        private System.Windows.Forms.Button BtnSplit;
+        private System.Windows.Forms.Button BtnNoofthe;
+    }
+}
+
---
+++ String_Manipulation/FrmStringManipulation.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace String_Manipulation
+{
+    public partial class FrmStringManipulation : Form
+    {
+        public FrmStringManipulation()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLength_Click(object sender, EventArgs e)
+        {
+            //Length
+            TxtResult.Text = TxtInput.Text.Length.ToString();
+        }
+
+        private void btnSubstring_Click(object sender, EventArgs e)
+        {
+            //Substring After 4
+            TxtResult.Text = TxtInput.Text.Substring(4);
+        }
+
+        private void BtnSubstring34_Click(object sender, EventArgs e)
+        {
+            TxtResult.Text = TxtInput.Text.Substring(3,4);
+        }
+
+        private void BtnStartsWith_Click(object sender, EventArgs e)
+        {
+            TxtResult.Text = TxtInput.Text.StartsWith("1").ToString();
+        }
+
+        private void BtnEndswith_Click(object sender, EventArgs e)
+        {
+            TxtResult.Text = TxtInput.Text.EndsWith("9").ToString();
+        }
+
+        private void BtnIndexof3_Click(object sender, EventArgs e)
+        {
+            TxtResult.Text = TxtInput.Text.IndexOf("3").ToString();
+        }
+
+        private void BtnToupper_Click(object sender, EventArgs e)
+        {
+            TxtResult.Text = TxtInput.Text.ToUpper();
+        }
+
+        private void BtnTolower_Click(object sender, EventArgs e)
+        {
+            TxtResult.Text = TxtInput.Text.ToLower();
+        }
+
+        private void BtnReplace_Click(object sender, EventArgs e)
+        {
+            TxtResult.Text = TxtInput.Text.Replace("1", "9");
+        }
+
+        private void BtnVowels_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+            string sentence = TxtInput.Text;
+            int length = sentence.Length;
+            for (int i = 0; i < length; i++)
+            {
+                string vowelCheck = sentence.Substring(i,1);
+                if(vowelCheck=="a" || vowelCheck == "e" || vowelCheck == "i" || vowelCheck == "o" || vowelCheck == "u" || vowelCheck == "A" || vowelCheck == "E" || vowelCheck == "I" || vowelCheck == "O" || vowelCheck == "U")
+                {
+                    count++;
+                }
+            }
+            MessageBox.Show($"There Are {count} Vowels In This Sentence");
+        }
+
+        //BtnSplit This Will Split 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //int v; 
+            string sentence = TxtInput.Text;
+            int length = sentence.Length;
+            for (int i = 0; i < length; i++)
+            {
+                string letter = sentence.Substring(i, 1);
+                if(letter == ",")
+                {
+                    MessageBox.Show("First Part Before Comma");
+                    string first = sentence.Substring(0 , i);
+                    MessageBox.Show(first);
+                    MessageBox.Show("After Comma");
+                    string second = sentence.Substring(i+1);
+                    MessageBox.Show(second);
+                }
+            }
+        }
+        private void BtnNoofthe_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+            string sentence = TxtInput.Text;
+            int length = sentence.Length;
+            
+            for (int i = 0; i < length-4; i++)
+            {
+                string letter = sentence.Substring(i, 5);
+                if (letter == " the " || letter == " The " || letter == "The " || letter == "the " || letter == " The" || letter == " the")
+                {
+                    count++;
+                }
+            }
+            MessageBox.Show($"No of The: {count}");
+        }
+    }
+}
